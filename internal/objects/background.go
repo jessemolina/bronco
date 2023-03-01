99010374b836c811c8e6d4ef2bcf439b9cedec0d
Jesse Molina <jesse@jessemolina.xyz>
2023-03-01 06:21:11 -0600

    Base background object implementation.

---
+++ internal/objects/background.go
@@ -0,0 +1,17 @@
+package objects
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func NewBackground(tx float64, ty float64) Object {
+	return nil
+}
+
+func Update(tick uint) error {
+	return nil
+}
+
+func Draw(*ebiten.Image) error {
+	return nil
+}
