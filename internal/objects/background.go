674280940062ae4beb5cb2a958317b40c2abef2c
Jesse Molina <jesse.molina@nextiva.com>
2023-03-04 11:33:11 -0600

    Scale background based on screen WxH.

--- internal/objects/background.go
+++ internal/objects/background.go
@@ -1,28 +1,24 @@
 package objects
 
 import (
-	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/jessemolina/bronco/internal/assets/images/background"
 )
 
-func NewBackground(tx float64, ty float64) Object {
+func NewBackground(screenWidth int, screenHeight int) Object {
+	frameW, frameH := background.Prairie.Image.Size()
 
-	w, h := background.Prairie.Image.Size()
-	frames := 1
-
-	w = w / frames
+	scale := float64(screenHeight) / float64(frameH)
 
 	bg := &Background{
 		img:         background.Prairie.Image,
-		frameWidth:  w,
-		frameHeight: h,
+		frameWidth:  frameW,
+		frameHeight: frameH,
 		frameX:      0,
 		frameY:      0,
-		targetX:     tx,
-		targetY:     ty,
+		scale:       scale,
 	}
 
 	return bg
@@ -37,15 +33,18 @@ type Background struct {
 	frameY      int // x + h
 	targetX     float64
 	targetY     float64
+	scale       float64
 }
 
 func (bg *Background) Update(tick uint) error {
 	pace, count := 5, 1
 	frame := (int(tick) / pace) % count
-	log.Println("tick: ", tick, "frame: ", frame)
+	log.Println("tick: ", tick, "frame: ", frame, "scale:", bg.scale,
+		"h:", float64(bg.frameHeight)*bg.scale,
+		"w:", float64(bg.frameWidth)*bg.scale,
+	)
 
-	bg.frameX = frame * bg.frameWidth + 10
-
+	bg.frameX = frame*bg.frameWidth + 10
 
 	return nil
 }
@@ -54,18 +53,21 @@ func (bg *Background) Draw(target *ebiten.Image) error {
 	// Options for drawing image
 	opts := &ebiten.DrawImageOptions{}
 	//opts.GeoM.Translate(bg.targetX, bg.targetY)
-	opts.GeoM.Scale(2, 2)
+	opts.GeoM.Scale(bg.scale, bg.scale)
 
-	x0, y0 := bg.frameX, bg.frameY
-	x1, y1 := x0 + bg.frameWidth - 100, y0 + bg.frameHeight
+	/*
+		x0, y0 := bg.frameX, bg.frameY
+		x1, y1 := x0 + bg.frameWidth - 100, y0 + bg.frameHeight
 
-	// Crop spritesheet
-	r := image.Rect(x0, y0, x1, y1)
+		// Crop spritesheet
+		r := image.Rect(x0, y0, x1, y1)
 
-	sub := bg.img.SubImage(r).(*ebiten.Image)
+		sub := bg.img.SubImage(r).(*ebiten.Image)
 
 
-	target.DrawImage(sub, opts)
+		target.DrawImage(sub, opts)
 
+	*/
+	target.DrawImage(bg.img, opts)
 	return nil
 }
