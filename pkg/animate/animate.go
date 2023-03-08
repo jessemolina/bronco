92abcd5dfc40acfbbc749ad570692e93632647b1
Jesse Molina <jesse@jessemolina.xyz>
2023-03-07 20:28:49 -0600

    Refactored to include ebiten image for drawing.

--- pkg/animate/animate.go
+++ pkg/animate/animate.go
@@ -2,10 +2,21 @@ package animate
 
 import (
 	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+
 const (
-		output = `
+
+	right direction = 1
+	left  direction = -1
+	down  direction = 1
+	up    direction = -1
+
+
+	output = `
 frameWidth, frameHeight:\t%v x %v
 frameX, frameY:\t%v x %v
 targetX, taregetY:\t%v x %v
@@ -14,37 +25,49 @@ pace:\t%v
 `
 )
 
-// Prints the animation struct on screen.
-func debugMessage(a *Animation) string {
-	message := fmt.Sprintf(output,
-		a.frameWidth, a.frameHeight,
-		a.frameX, a.frameY,
-		a.targetX, a.targetY,
-		a.scale,
-		a.pace,
-	)
+type direction int
 
-	return message
+func (d direction) invert() direction {
+	return -d
 }
 
 type Animation struct {
-	frameWidth  int // frame width
-	frameHeight int // frame height
-	frameX      int // x coordinate on frame
-	frameY      int // y coordinate on frame
-	targetX     float64 // x coordinate on target (i.e. screen)
-	targetY     float64 // y coordinate on target (i.e. screen)
-	scale       float64 // scale of image
-	pace        float64
+	Img         *ebiten.Image
+	FrameWidth  int     // frame width
+	FrameHeight int     // frame height
+	FrameX      int     // x coordinate on frame
+	FrameY      int     // y coordinate on frame
+	TargetX     float64 // x coordinate on target (i.e. screen)
+	TargetY     float64 // y coordinate on target (i.e. screen)
+	Scale       float64 // scale of image
+	Pace        float64
 }
+
+// Prints the animation struct on screen.
+func (a *Animation) DebugMessage(target *ebiten.Image, targetX int, targetY int) {
+	message := fmt.Sprintf(output,
+		a.FrameWidth, a.FrameHeight,
+		a.FrameX, a.FrameY,
+		a.TargetX, a.TargetY,
+		a.Scale,
+		a.Pace,
+	)
+	ebitenutil.DebugPrintAt(target, message, targetX, targetY)
+}
+
 
 // animate
 
 // move
 
 // multiply
-func (a *Animation) multiply() {
-
+func (a *Animation) multiply(target *ebiten.Image, count int) {
+	for i := 0; i < count; i++ {
+		opts := &ebiten.DrawImageOptions{}
+		tx := float64(a.FrameWidth * i)
+		opts.GeoM.Translate(tx, a.TargetY)
+		target.DrawImage(a.Img, opts)
+	}
 }
 
 // scroll (direction, pace)
