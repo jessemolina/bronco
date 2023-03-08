fc61aa12073f2b2e03b72a426ae019961367d568
Jesse Molina <jesse@jessemolina.xyz>
2023-03-07 22:23:17 -0600

    Working scrolling tiles.

--- internal/objects/tiles.go
+++ internal/objects/tiles.go
@@ -1,10 +1,10 @@
 package objects
 
 import (
-	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/jessemolina/bronco/internal/assets/images/tiles"
+	"github.com/jessemolina/bronco/pkg/animate"
 )
 
 func NewTiles(screenWidth int, screenHeight int) Object {
@@ -12,13 +12,21 @@ func NewTiles(screenWidth int, screenHeight int) Object {
 
 	targetY := float64(screenHeight - frameH)
 	targetX := float64(0)
+	//targetX := float64(screenWidth / 2)
+
+	anm := &animate.Animation{
+		Img:         tiles.Prairie02.Image,
+		FrameWidth:  frameW,
+		FrameHeight: frameH,
+		TargetX:     targetX,
+		TargetY:     targetY,
+		Pace: 2,
+	}
 
 	bg := &Tiles{
-		img:         tiles.Prairie02.Image,
-		frameWidth:  frameW,
-		frameHeight: frameH,
-		targetX:     targetX,
-		targetY:     targetY,
+		anm,
+		screenWidth,
+		screenHeight,
 	}
 
 	return bg
@@ -26,44 +34,41 @@ func NewTiles(screenWidth int, screenHeight int) Object {
 
 // Import images that are already decoded.
 type Tiles struct {
-	img         *ebiten.Image
-	frameWidth  int // frame width
-	frameHeight int // frame height
-	frameX      int // starting point
-	frameY      int // x + h
-	targetX     float64
-	targetY     float64
-	scale       float64
+	animate *animate.Animation
+	screenWidth int
+	screenHeight int
 }
 
 func (t *Tiles) Update(tick uint) error {
-	pace, count := 5, 1
-	frame := (int(tick) / pace) % count
-	log.Println("tick: ", tick, "frame: ", frame, "scale:", t.scale,
-		"h:", float64(t.frameHeight)*t.scale,
-		"w:", float64(t.frameWidth)*t.scale,
-	)
+	t.animate.UpdateScrollWidth(t.screenWidth, -1)
+	/*
+		pace, count := 5, 1
+		frame := (int(tick) / pace) % count
+		log.Println("tick: ", tick, "frame: ", frame, "scale:", t.scale,
+			"h:", float64(t.frameHeight)*t.scale,
+			"w:", float64(t.frameWidth)*t.scale,
+		)
 
-	t.frameX = frame*t.frameWidth + 10
+		t.frameX = frame*t.frameWidth + 10
+	*/
 
 	return nil
 }
 
 func (t *Tiles) Draw(target *ebiten.Image) error {
 	// Options for drawing image
-	//	opts.GeoM.Translate(t.targetX, t.targetY)
-
+	/*
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(t.anm.TargetX, t.anm.TargetY)
+	target.DrawImage(t.anm.Img, opts)
+	*/
 	targetW, _ := target.Size()
-	repeat := targetW / t.frameWidth
+	repeat := (targetW / t.animate.FrameWidth) * 2
 
-	for j := 0; j < repeat; j++ {
-		opts := &ebiten.DrawImageOptions{}
-		tx := float64(t.frameWidth * j)
-		opts.GeoM.Translate(tx, t.targetY)
+	t.animate.DrawSequenceX(target, repeat)
 
-		target.DrawImage(t.img, opts)
+		//
 
-	}
 	//opts.GeoM.Scale(2, 2)
 
 	/*
