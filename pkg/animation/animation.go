4de830864ca82dfabd1e402400acb1f0ae0c7a3b
Jesse Molina <jesse@jessemolina.xyz>
2023-03-07 18:59:01 -0600

    Basic output debug message func.

--- pkg/animation/animation.go
+++ pkg/animation/animation.go
@@ -1,8 +1,34 @@
 package animation
 
 import (
+	"fmt"
+
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+const (
+		output = `
+frameWidth, frameHeight:\t%v x %v
+frameX, frameY:\t%v x %v
+targetX, taregetY:\t%v x %v
+scale:\t%v
+pace:\t%v
+`
+)
+
+// Prints the animatin struct on screen.
+func (a *Animation)debugPrint(img *ebiten.Image) {
+	message := fmt.Sprintf(output,
+		a.frameWidth, a.frameHeight,
+		a.frameX, a.frameY,
+		a.targetX, a.targetY,
+		a.scale,
+		a.pace,
+	)
+
+	ebitenutil.DebugPrint(img, message)
+}
 
 type Animation struct {
 	img         *ebiten.Image
@@ -15,3 +41,16 @@ type Animation struct {
 	scale       float64 // scale of image
 	pace        float64
 }
+
+// animate
+
+// move
+
+// multiply
+func (a *Animation) multiply() {
+
+}
+
+// scroll (direction, pace)
+
+// jump
