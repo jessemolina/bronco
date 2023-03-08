3e7cb08473b91a8a1bbf559010afbbd22be88a2b
Jesse Molina <jesse@jessemolina.xyz>
2023-03-07 20:28:22 -0600

    Removed object direction and output message.

--- internal/objects/objects.go
+++ internal/objects/objects.go
@@ -4,25 +4,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-type direction int
 
-const (
-	right direction = 1
-	left  direction = -1
-	down  direction = 1
-	up    direction = -1
-
-	output = `
-frameWidth, frameHeight: %v x %v
-frameX, frameY: %v x %v
-targetX, targetY: %v x %v
-scale: %v
-`
-)
-
-func (d direction) invert() direction {
-	return -d
-}
 type Object interface {
 	Update(uint) error
 	Draw(*ebiten.Image) error
