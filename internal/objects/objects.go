6f05b69516ab9df3b8c47e47498d15ce49a95604
Jesse Molina <jesse@jessemolina.xyz>
2023-03-09 00:36:15 -0600

    Object interface for providing coordinates.

--- internal/objects/objects.go
+++ internal/objects/objects.go
@@ -1,11 +1,13 @@
 package objects
 
 import (
+	"image"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
-
 
 type Object interface {
 	Update(uint) error
 	Draw(*ebiten.Image) error
+	Coordinates() image.Rectangle
 }
