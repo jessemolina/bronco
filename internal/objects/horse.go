c9467ab12b362fe81611ef1ae11663eaac8d26ba
Jesse Molina <jesse@jessemolina.xyz>
2023-03-08 19:41:56 -0600

    Updated horse to use animate pkg.

--- internal/objects/horse.go
+++ internal/objects/horse.go
@@ -1,11 +1,14 @@
 package objects
 
 import (
+	"fmt"
 	"image"
 	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/jessemolina/bronco/internal/assets/images/horse"
+	"github.com/jessemolina/bronco/pkg/animate"
 )
 
 var message = `g.count: %v
@@ -16,34 +19,37 @@ size: %v, %v
 `
 
 // TODO replace tx tx parameters for new horse
-func NewHorse(tx float64, ty float64) Object {
+func NewHorse(screenWidth int, screenHeight int) Object {
 
-	w, h := horse.Walk.Image.Size()
+	frameW, frameH := horse.Walk.Image.Size()
 
-	w = w / horse.Walk.Specs.Frames
+	frameW = frameW / horse.Walk.Specs.Frames
 
-	horse := &Horse{
-		img:         horse.Walk.Image,
-		frameWidth:  w,
-		frameHeight: h,
-		frameX:      0,
-		frameY:      0,
-		targetX:     tx,
-		targetY:     ty,
+	animate := &animate.Animation{
+		Img:         horse.Walk.Image,
+		FrameWidth:  frameW,
+		FrameHeight: frameH,
+		FrameX:      0,
+		FrameY:      0,
+		TargetX:     50,
+		TargetY:     185,
 	}
 
-	return horse
+	h := &Horse{
+		animate: animate,
+		screenWidth: screenWidth,
+		screenHeight: screenHeight,
+	}
+
+	return h
 }
 
 // Import images that are already decoded.
 type Horse struct {
-	img         *ebiten.Image
-	frameWidth  int // frame width
-	frameHeight int // frame height
-	frameX      int // starting point
-	frameY      int // x + h
-	targetX     float64
-	targetY     float64
+	animate      *animate.Animation
+	screenWidth  int
+	screenHeight int
+	isJump       bool
 }
 
 // Horse type with image and position.
@@ -56,7 +62,11 @@ func (h *Horse) Update(tick uint) error {
 	frame := (int(tick) / pace) % count
 	//log.Println("tick: ", tick, "frame: ", frame)
 
-	h.frameX = frame * h.frameWidth
+	h.animate.FrameX = frame * h.animate.FrameWidth
+
+	// TODO catch key press
+	key := ebiten.KeySpace
+	h.isJump = ebiten.IsKeyPressed(key)
 
 	return nil
 }
@@ -67,22 +77,30 @@ func (h *Horse) Draw(target *ebiten.Image) error {
 	// Options for drawing image
 	opts := &ebiten.DrawImageOptions{}
 	// TODO dynamically update the targetX and Y for horse
-	opts.GeoM.Translate(50, 185)
+	if h.isJump {
+		opts.GeoM.Translate(h.animate.TargetX, h.animate.TargetY-30)
+	} else {
+		opts.GeoM.Translate(h.animate.TargetX, h.animate.TargetY)
+	}
+
 	opts.GeoM.Scale(2, 2)
 
 	// coordinates
 	// x0, y0 := int(h.x), int(h.x)+h.height
 	// x1, y1 := x0 + h.width, y0 + h.height
 	//
-	x0, y0 := h.frameX, h.frameY
-	x1, y1 := x0 + h.frameWidth, y0 + h.frameHeight
+	x0, y0 := h.animate.FrameX, h.animate.FrameY
+	x1, y1 := x0+h.animate.FrameWidth, y0+h.animate.FrameHeight
 
 	// Crop spritesheet
 	r := image.Rect(x0, y0, x1, y1)
 
-	sub := h.img.SubImage(r).(*ebiten.Image)
+	sub := h.animate.Img.SubImage(r).(*ebiten.Image)
 
 	target.DrawImage(sub, opts)
+
+	message := fmt.Sprintf("jump: %v", h.isJump)
+	ebitenutil.DebugPrint(target, message)
 
 	return nil
 }
